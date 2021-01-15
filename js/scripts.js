a360823f7cf9699a4d70ea393fb7a4f31f6dc60e
HR Williams <halrubinwilliams@gmail.com>
2021-01-15 14:04:40 -0800

    Show I have been working, scripts still in progress, css commented out for test

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 function results(input1, input2, input3, input4, input5) {
   const total = input1 + input2 + input3 + input4 + input5;
-}
 
   if(total <=5) {
     language = "ruby";
@@ -12,3 +11,35 @@ function results(input1, input2, input3, input4, input5) {
     return language;
 }
 
+$(document).ready(function() {
+  $("form#languageQuiz").submit(function() {
+    event.preventDefault();
+
+    const syntax = parseInt($("#syntax").val();
+    const software = parseInt($("#software").val();
+    const employer = parseInt($("#employer").val();
+    const con = parseInt($("#con").val();
+    const bonus = parseInt($("#bonus").val();
+    let language = results(syntax, software, employer, con, bonus);
+
+    if (language === "ruby") {
+      $("#answer").text("Ruby");
+    } else if (language === "go") {
+      $("answer").text("Go");
+    } else {
+      $("#answer").text(
+        "Swift");
+      )
+    }
+    )
+    
+    
+    )
+    
+    
+    )
+    
+    
+    ))
+  })
+})
