bd642a1d658f999feb049689a1dbf2bea647947f
HR Williams <halrubinwilliams@gmail.com>
2021-01-15 13:35:09 -0800

    Change form id, start business logic

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function results(input1, input2, input3, input4, input5) {
+  const total = input1 + input2 + input3 + input4 + input5;
+}
+
+  if(total <=5) {
+    language = "ruby";
+  } else if (total <=10) {
+    language = "go";
+  } else {
+    langage = "swift";
+  }
+    return language;
+}
+
